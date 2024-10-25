31ea59b403a3c6dae7c8911f64c10f637d99fcb4
Benjamin Perez <benjamin@bexsoft.net>
2024-10-25 14:45:40 -0600

    test1

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -13,6 +13,7 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'use client'
 
 import React, { FC, Fragment, useMemo } from "react";
 import { css, useTheme } from "@emotion/react";
