4f2bc706a5fa368294c129542905938d5e49a301
Benjamin Perez <benjamin@bexsoft.net>
2025-03-24 15:51:53 -0600

    Updated 1

--- src/components/Icons/Icons.stories.tsx
+++ src/components/Icons/Icons.stories.tsx
@@ -1601,11 +1601,6 @@ const Template: Story = (args) => {
               <br />
               FolderBrowserIcon
             </div>
-            <div className="story-icon">
-              <ficons.TrashOffIcon />
-              <br />
-              TrashOffIcon
-            </div>
           </IconDisplay>
         </Grid>
       </Fragment>
